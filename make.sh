74ccfb78f5e813da80117b71ef4877f5ec778584
gmoraesalvarez <moraes.alvarez@gmail.com>
2013-03-14 21:05:54 -0300

    fpc make commit

---
+++ make.sh
@@ -0,0 +1,1 @@
+fpc -MObjFPC -Scghi -CX -Os3 -Xs -XX -vewnhi -Filib/x86_64-linux -Fl/opt/gnome/lib -Fu../../.lazarus/lib/units/x86_64-linux/gtk2 -Fu../../.lazarus/lib/LCLBase/units/x86_64-linux -Fu../../.lazarus/lib/LazUtils/lib/x86_64-linux -Fu../../.lazarus/lib/units/x86_64-linux -Fu. -FUlib/x86_64-linux/ -l -dLCL -dLCLgtk2 gmawallpaper.lpr
